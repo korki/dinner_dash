2a25f42efa39bc89660af0c1b1d01e9f66f37139
Jose Pettoruti <jose.pettoruti@surfdome.com>
2014-02-05 12:18:13 +0000

    rails environment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DinnerDash::Application.config.secret_key_base = '8092c9eda026684436deed7e292595567b5702093bbe75a5da092dc586e311605c1af101eba566005fd5f94d5f0c7825795c087de842b417bfd14317e528de67'
