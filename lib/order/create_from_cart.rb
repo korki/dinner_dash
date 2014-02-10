b26877e6c13a53a4075a083262ac967da73f47f7
Orest Hrycyna <orest.hrycyna@surfdome.com>
2014-02-10 11:22:32 +0000

    Fix status when order placed

--- lib/order/create_from_cart.rb
+++ lib/order/create_from_cart.rb
@@ -6,7 +6,7 @@ require 'order_item'
 class Order
   class CreateFromCart
     def self.call cart
-      order = Order.create(user_id: cart.user_id, status: 1)
+      order = Order.create(user_id: cart.user_id, status: 'ordered')
       cart.cart_items.each do |cart_item|
         Order::AddItem.call order, cart_item.item, cart_item.quantity
       end
