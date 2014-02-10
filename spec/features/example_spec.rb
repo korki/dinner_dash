94b5f1af958110c60a5242012698f6ed80c818da
Josh Cheek <josh.cheek@gmail.com>
2014-02-10 12:05:05 +0000

    Delete irrelevant spec

--- spec/features/example_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe 'example that navigates the app' do
-  it 'can hit the root of my app' do
-    visit root_url
-    expect(page).to have_content 'Dinner Dash'
-    expect(page.status_code).to eq 200
-  end
-end
