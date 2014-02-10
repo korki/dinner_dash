32d1e2a4418da62935039c0be87c03a0b8a680a9
Josh Cheek <josh.cheek@gmail.com>
2014-02-10 12:12:30 +0000

    Refactor/advice on the logins_spec

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -3,27 +3,32 @@ require 'capybara'
 
 describe 'check user' do
   let :user do
-      User.create(full_name: 'tony', display_name: 'tonyk', email: 't@t.com', password:'password',  password_confirmation:'password') 
+      User.create(full_name: 'tony',
+                  display_name: 'tonyk',
+                  email: 't@t.com',
+                  password:'password',
+                  password_confirmation:'password')
    end
- 
+
+  # ??
   it 'add user' do
     expect(user.full_name).to eq 'tony'
   end
 
-  it 'get login page' do
-    visit login_url
-    expect(page.status_code).to eq 200
-  end
-
+  # probably change these to be
+  # that I can login as a normal user and an admin user
   it 'login' do
     visit login_url
+    expect(page.status_code).to eq 200
     fill_in 'email', :with => user.email
     fill_in 'password', :with => 'password'
     click_on 'Login'
-    page.should have_content('Logged in as t@t.com')  
+    page.should have_content('Logged in as t@t.com')
   end
 
-  it 'check user role' do 
+  # probably change this to check that after logging in with
+  # an admin user, I'm on an admin page
+  it 'check user role' do
     visit login_url
     fill_in 'email', :with => user.email
     fill_in 'password', :with => 'password'
@@ -38,9 +43,9 @@ describe 'check user' do
     fill_in 'email', :with => user.email
     fill_in 'password', :with => 'password'
     click_on 'Login'
-    page.should have_content('Logged in as t@t.com') 
-    visit logout_url 
-    page.should have_content('Logged out')  
+    page.should have_content('Logged in as t@t.com')
+    visit logout_url
+    page.should have_content('Logged out')
   end
 end
- 
+
